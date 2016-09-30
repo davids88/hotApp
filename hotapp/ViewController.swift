a2c1be78229daaacbff0515f4dc84b00e0714109
davids88 <bokka18@hotmail.com>
2016-09-30 21:23:20 +1000

    changes
    changed spelling

--- hotapp/ViewController.swift
+++ hotapp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var hi = "Hellow Worlds"
+        var hi = "Hello Worlds"
         
     }
 
